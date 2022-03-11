ff246472584d9eac51b0151f16257acd7ce06059
Kalliu Brasil <kalliub@gmail.com>
2022-03-11 16:50:09 -0300

    Adding support to items details

--- api/controllers/PokemonController.js
+++ api/controllers/PokemonController.js
@@ -8,25 +8,33 @@
 module.exports = {
   getPokemon: async (req, res) => {
     const pokemonName = String(req.param("name")).toLowerCase();
-    const pokeApiUrl = "https://pokeapi.co/api/v2/";
+    const pokeApiUrl = "https://pokeapi.co/api/v2";
 
     if (!pokemonName || pokemonName.length === 0) {
       return res.badRequest("Invalid pokemon");
     }
 
     // Getting data from PokeAPI
-    let pokeApiRes = await sails.fetch(`${pokeApiUrl}/pokemon/${pokemonName}`);
-    if (pokeApiRes.status === 404) {
+    let pokemonData = await sails.fetch(`${pokeApiUrl}/pokemon/${pokemonName}`);
+    if (pokemonData.status === 404) {
       return res.status(404).json("Pokemon not found!");
     }
-    if (pokeApiRes.status >= 300) {
+    if (pokemonData.status >= 300) {
       return res.serverError("There's a problem with the Pokemon Database");
     }
 
-    pokeApiRes = await pokeApiRes.json();
+    pokemonData = await pokemonData.json();
+
+    // Getting items details
+    const pokemonItems = pokemonData.held_items;
+    const itemsData = await Promise.all(
+      pokemonItems.map(async ({ item }) => {
+        return sails.fetch(item.url).then((r) => r.json());
+      })
+    );
 
     // Sorting Abilities
-    const sortedAbilities = pokeApiRes.abilities;
+    const sortedAbilities = pokemonData.abilities;
     sortedAbilities.sort((a, b) => {
       if (a.ability.name < b.ability.name) {
         return -1;
@@ -36,8 +44,9 @@ module.exports = {
 
     // Final JSON
     return res.ok({
-      ...pokeApiRes,
+      ...pokemonData,
       abilities: sortedAbilities,
+      held_items: itemsData,
     });
   },
 };
